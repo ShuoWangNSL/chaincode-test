6822c24b93c9536720012cb80b31a79c327845b9
Shuo Wang <wangs16@mails.tsinghua.edu.cn>
2019-04-20 10:43:38 -0700

    Rename donothing.go to loop.go

---
+++ donothing/loop.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+  "time"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+type DoNothing struct {
+}
+
+func main() {
+	err := shim.Start(new(DoNothing))
+	if err != nil {
+		fmt.Printf("Error starting donothing: %s", err)
+	}
+}
+
+func (t *DoNothing) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, nil
+}
+
+func (t *DoNothing) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	if function == "donothing" {
+    temp := 1
+    for {
+      temp = temp + 1
+      temp = temp - 1
+      time.Sleep(1 * time.Millisecond)
+    }
+		return nil, nil
+	}
+	return nil, errors.New("Received unknown function invocation")
+}
+
+func (t *DoNothing) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	return nil, errors.New("Received unknown function query")
+}
